8b409a41d7743abe8456d57d972be45c96f6eab6
Lu junru <lujunru31415926@163.com>
2024-06-17 14:29:18 +0800

    Release clean codes

---
+++ tasks.sh
@@ -0,0 +1,44 @@
+# DPO
+bash dpo.sh 0 sigmoid
+
+# Iterative DPO
+bash dpo_iterative.sh 0 sigmoid
+
+# IPO
+bash dpo.sh 0 ipo
+
+# KTO
+bash dpo.sh 0 kto_pair
+
+# Hybrid DPO+SFT
+bash dpo.sh 0 mix
+
+# DPOP
+bash dpo.sh 0 dpop
+
+# Lenth-normed DPO
+bash dpo.sh 0 lenN
+
+# ORPO
+bash dpo.sh 0 orpo
+
+# TDPO 2
+bash dpo.sh 0 tdpo
+
+# BCO
+bash dpo.sh 0 bco_pair
+
+# SPPO
+bash dpo.sh 0 sppo_hard
+
+# SimPO
+bash dpo.sh 0 simpo
+
+# NCA
+bash dpo.sh 0 nca
+
+# SamPO
+bash dpo.sh 1 sigmoid
+
+# Iterative SamPO
+bash dpo_iterative.sh 1 sigmoid
